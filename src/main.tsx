adb3fdc1f5f6df0f9ffd01e8fe6119d0ac793195
XaulimMatadordePorco244 <dmichaeldouglas00@gmail.com>
2025-06-02 11:47:26 -0400

    Criando o meu primeiro componente

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// import './index.css'
+import Footer from './footer.tsx'
+import Header from './header.tsx'
+import Body from './body.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Footer/>
+    <Body/>
+    <Header/>
   </StrictMode>,
 )
