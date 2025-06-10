d3d2d9c551426a0257d51973854ae2d1c119180f
XaulimMatadordePorco244 <dmichaeldouglas00@gmail.com>
2025-06-10 07:38:24 -0400

    Exemplo de useState

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,38 +1,20 @@
-//Prop -> é utilizado para passar um valor diferente para cada componente em sua criação
+import { useState } from "react"
 
-//States -> Estados, slavam estados de variáveis do react e quando esses estados mudam. 
-//Automaticamente o React entende que ele precisa atualizar a página para mostrar os novos valores do componente
-import React from 'react'
-
-interface ContainerProps{
-    nome:string
-    
+interface ContainerProps {
+    nome: string
 }
-interface ContainerInfoState{
-    nome:string
-}
-
-
-class Container extends React.Component<ContainerProps, ContainerInfoState>{
-    constructor(props:ContainerProps){
-        super(props)
-        this.state = {
-            nome:"Nome antes de Mudar"
-        }
+function Container(props: ContainerProps) {
+    const [texto,setTexto] = useState("Texto antes de mudar")
+    function mudar(){
+        setTexto("Texto depois de mudar")
     }
-    mudar = ()=>{
-        this.setState({nome:"Nome depois de Mudar"})
-    }
-    render(): React.ReactNode{
-        return (
+    return (
         <>
-        <h1>{this.props.nome}</h1>
-        <p >{this.state.nome}</p>
-        <button onClick={this.mudar}>Mudar Estado</button>
+            <h1>{props.nome}</h1>
+            <p>{texto}</p>
+            <button onClick={mudar}>Mudar</button>
         </>
     )
-    }
-
 }
 
 export default Container
